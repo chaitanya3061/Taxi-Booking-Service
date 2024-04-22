5bc809176e7ad3adff9c5a1974db5df7b0416b02
nomula.s <nomula.s@intimetec.com>
2024-04-22 23:40:56 +0530

    added API layer

---
+++ TaxiBookingService.API/User/Driver/DriverTaxiServiceContracts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiBookingService.API.User.Driver
+{
+    public class DriverTaxiServiceContracts
+    {
+        public string Name { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Color { get; set; }
+        public string Model { get; set; }
+        public string TaxiType { get; set; } //dtos
+    }
+}
