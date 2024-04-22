b402991f432350705b94733fc8a41085c6c492e6
nomula.s <nomula.s@intimetec.com>
2024-04-22 23:47:54 +0530

    added Host layer

---
+++ TaxiBookingService.Host/ServiceCollection.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+using TaxiBookingService.Client.Geocoding;
+using TaxiBookingService.Client.Geocoding.Interfaces;
+using TaxiBookingService.Common.Utilities;
+using TaxiBookingService.Dal;
+using TaxiBookingService.Dal.Entities;
+using TaxiBookingService.Dal.Interfaces;
+using TaxiBookingService.Dal.Repositories;
+using TaxiBookingService.Logic.User;
+using TaxiBookingService.Logic.User.Interfaces;
+
+namespace TaxiBookingService.Host
+{
+    public static class ServiceCollecction
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddAutoMapper(typeof(Program));
+            services.AddHttpClient("ExternalApi", client =>
+            {
+                client.BaseAddress = new Uri("https://api.geoapify.com/v1/geocode");
+            });
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization header using bearer scheme(\"bearer{token}\")",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+            });
+
+            services.AddHttpContextAccessor();
+            services.AddSingleton<ILoggerAdapter, SerilogAdapter>();
+            services.AddTransient<IDriverLogic<Driver>, DriverLogic>();
+            services.AddTransient<IDriverRepository<Driver>, DriverRepository>();
+            services.AddTransient<IExternalHttpClient, ExternalApiClient>();
+            services.AddTransient<ICustomerLogic<Customer>, CustomerLogic>();
+            services.AddTransient<ICustomerRepository<Customer>, CustomerRepository>();
+            //services.AddTransient<IAdminLogic<Admin>, AdminLogic>();
+            //services.AddTransient<IAdminRepository<Admin>, AdminRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("AppSettings:Token").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddDbContext<TaxiBookingServiceDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("TaxiBookingServiceDbContext")));
+        }
+    }
+}
