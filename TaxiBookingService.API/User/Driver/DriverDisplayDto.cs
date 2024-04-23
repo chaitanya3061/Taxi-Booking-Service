31bf8970d59e47856f277e4abaca7fe2b53e196c
nomula.s <nomula.s@intimetec.com>
2024-04-23 09:31:35 +0530

    added driverdisplay dto

---
+++ TaxiBookingService.API/User/Driver/DriverDisplayDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiBookingService.API.User.Driver
+{
+    public class DriverDisplayDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public float DriverRating { get; set; }
+        public string LicenceNumber { get; set; }
+
+    }
+}
