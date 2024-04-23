5efa2f894c359e0ea8e9a1553f94a24097e35b93
nomula.s <nomula.s@intimetec.com>
2024-04-23 11:02:50 +0530

    added admin driver rating dto

---
+++ TaxiBookingService.API/User/Driver/DriverRatingDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiBookingService.Common.Attributes;
+
+namespace TaxiBookingService.API.User.Driver
+{
+    public class DriverRatingDto
+    {
+        [PositiveIntegerValidation]
+        public int RideId { get; set; }
+
+        public string Feedback { get; set; }
+
+        public float Rating { get; set; }
+    }
+}
