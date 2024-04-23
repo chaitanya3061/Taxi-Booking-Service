941e202c40133e8fc738db66ab257e6360b13326
nomula.s <nomula.s@intimetec.com>
2024-04-23 11:25:07 +0530

    modified driver login dto

--- TaxiBookingService.API/User/Driver/DriverLoginDto.cs
+++ TaxiBookingService.API/User/Driver/DriverLoginDto.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TaxiBookingService.Common.Attribute;
 using TaxiBookingService.Common.Attributes;
 
 namespace TaxiBookingService.API.User.Driver
 {
     public class DriverLoginDto
     {
-        [EmailValidationAttribute]
+        [EmailValidation]
         public string Email { get; set; }
 
-        [RequiredValidationAttribute]
+        [RequiredValidation]
         public string Password { get; set; }
     }
 }
