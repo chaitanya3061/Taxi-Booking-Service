3303daaa6e18866728f36c2a7498586efb1b6446
nomula.s <nomula.s@intimetec.com>
2024-04-23 11:02:05 +0530

    added admin driver get ride dto

---
+++ TaxiBookingService.API/User/Driver/DriverGetRideDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiBookingService.API.User.Driver
+{
+    public class DriverGetRideDto
+    {
+        public string TaxiType { get; set; }
+        public string PickupLocation { get; set; }
+        public string DropoffLocation { get; set; }
+        public Decimal TotalFare { get; set; }
+    }
+}
