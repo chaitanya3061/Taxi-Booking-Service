98a49778174e7f36c9fdadd5284b6aea19255c6a
nomula.s <nomula.s@intimetec.com>
2024-04-23 11:23:41 +0530

    modified admin login dto

--- TaxiBookingService.API/User/Admin/AdminLoginDto.cs
+++ TaxiBookingService.API/User/Admin/AdminLoginDto.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaxiBookingService.Common.Attributes;
 
 namespace TaxiBookingService.API.User.Admin
 {
     public class AdminLoginDto
     {
+        [EmailValidation]
         public string Email { get; set; }
+
+        [RequiredValidation]
         public string Password { get; set; }
     }
 }
