bbddfb7e2ac82873d9f8cc85f34d96b4b0734f0a
nomula.s <nomula.s@intimetec.com>
2024-04-23 10:59:55 +0530

    added admin customer change payment dto

---
+++ TaxiBookingService.API/User/Customer/CustomerChangePaymentTypeDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiBookingService.Common.Attributes;
+
+namespace TaxiBookingService.API.User.Customer
+{
+    public class CustomerChangePaymentTypeDTO
+    {
+        [PositiveIntegerValidation]
+        public int CustomerId { get; set; }
+
+
+        [RequiredValidation]
+        public string PaymentType { get; set; }
+    }
+}
