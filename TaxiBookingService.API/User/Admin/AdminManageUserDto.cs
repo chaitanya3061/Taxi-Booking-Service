3c5ce2383c798baa46de4940a84090498a0a0951
nomula.s <nomula.s@intimetec.com>
2024-04-23 10:59:24 +0530

    added admin manage user dto

---
+++ TaxiBookingService.API/User/Admin/AdminManageUserDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiBookingService.Common.Attributes;
+
+namespace TaxiBookingService.API.User.Admin
+{
+    public class AdminManageUserDto
+    {
+        [RequiredValidation]
+        public string Name { get; set; }
+
+
+        [EmailValidation]
+        public string Email { get; set; }
+
+
+        [RequiredValidation]
+        public string CountryCode { get; set; }
+
+
+        [PhoneNumberValidation]
+        public string PhoneNumber { get; set; }
+    }
+}
