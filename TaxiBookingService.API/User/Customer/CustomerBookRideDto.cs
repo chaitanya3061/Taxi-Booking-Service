f119eb8a3c031088c25a7c86d8c827852ffb14bf
nomula.s <nomula.s@intimetec.com>
2024-04-23 09:29:15 +0530

    added customerbook dto

---
+++ TaxiBookingService.API/User/Customer/CustomerBookRideDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiBookingService.Common.Attributes;
+
+namespace TaxiBookingService.API.User.Customer
+{
+    public class CustomerBookRideDto
+    {
+        [LocationValidationAttribute]
+        public string PickupLocation { get; set; }
+
+        [LocationValidationAttribute]
+        [Compare(nameof(PickupLocation), ErrorMessage = "locations mismatch")]
+        public string DropoffLocation { get; set; }
+
+        [RequiredValidationAttribute]
+        public string TaxiType { get; set; }
+
+        [RequiredValidationAttribute]
+        public string PaymentType { get; set; }
+    }
+}
