2748cc01a27dc8c96c6ca00e3804798b5547ab96
nomula.s <nomula.s@intimetec.com>
2024-04-23 09:32:59 +0530

    added drivertaxi dto

---
+++ TaxiBookingService.API/User/Driver/DriverTaxiDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiBookingService.Common.Attributes;
+
+namespace TaxiBookingService.API.User.Driver
+{
+    public class DriverTaxiDto
+    {
+        [RequiredValidationAttribute]
+        public string Name { get; set; }
+
+
+        [RegistrationNumberAttribute]
+        public string RegistrationNumber { get; set; }
+
+        [RequiredValidationAttribute]
+        public string Color { get; set; }
+
+        [RequiredValidationAttribute]
+        public string TaxiType { get; set; } 
+    }
+}
